7106e54ad02bba7afd1a203c181bf77a65a3da16
abeiseke <student@ALEM-F4-R06-03.alem.school>
2025-08-21 14:04:18 +0600

    add env file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package rsshub
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 	"rsshub/internal/adapter/postgre"
@@ -9,7 +10,19 @@ import (
 )
 
 func main() {
-	connStr := "host=localhost port=5432 user=rsshub-db password=rsspass dbname=rsshub sslmode=disable"
+	dbHost := os.Getenv("POSTGRES_HOST")
+	dbPort := os.Getenv("POSTGRES_PORT")
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPass := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DBNAME")
+
+	//	interval := os.Getenv("CLI_APP_TIMER_INTERVAL")
+	//	workers := os.Getenv("CLI_APP_WORKERS_COUNT")
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPass, dbName,
+	)
+
 	pgAdapter, err := postgre.NewApiAdapter(connStr)
 	if err != nil {
 		slog.Error("Postgres connection error", "err", err)
--- migration/create_feeds_table.up.sql
+++ migration/create_feeds_table.up.sql
@@ -1,17 +1,20 @@
 -- для самих каналов
 create table feeds (
     id serial primary key,
-    title text not null,
-    link text not null,
-    description text
+    name text not null,
+    url text not null,
+    created_at timestamp not null ,
+    updated_at timestamp not null
 );
 
 -- для самих статей
-create table items (
+create table articles (
     id serial primary key,
     feed_id int references feeds(id) on delete cascade,
     title text not null,
     link text not null,
-    description text,
-    pub_date timestamp
+    published_at timestamp,
+    description text not null ,
+    created_at timestamp,
+    updated_at timestamp
 );
